8bba261a79ec69e4c2e7ab144c9fe6ea0cb812d0
Lucas Graybuck <lucasg@alleninstitute.org>
2019-10-15 16:35:24 -0700

    updates to argument handling and adding a wrapper script to make this more command-line compatible

---
+++ run_hto_processing.R
@@ -0,0 +1,11 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+rmarkdown::render(
+  input = "hto_processing.Rmd",
+  output_file = args[6],
+  params = c(in_type = args[1],
+             in_file = args[2],
+             in_key  = args[3],
+             out_mat = args[4],
+             out_tbl = args[5])
+)
