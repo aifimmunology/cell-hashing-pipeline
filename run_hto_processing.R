934c7d987eade216144761ba866f261f6e3a6075
Lucas Graybuck <lucasg@alleninstitute.org>
2019-10-15 17:17:12 -0700

    c -> list

--- run_hto_processing.R
+++ run_hto_processing.R
@@ -3,9 +3,9 @@ args <- commandArgs(trailingOnly = TRUE)
 rmarkdown::render(
   input = "hto_processing.Rmd",
   output_file = args[6],
-  params = c(in_type = args[1],
-             in_file = args[2],
-             in_key  = args[3],
-             out_mat = args[4],
-             out_tbl = args[5])
+  params = list(in_type = args[1],
+                in_file = args[2],
+                in_key  = args[3],
+                out_mat = args[4],
+                out_tbl = args[5])
 )
