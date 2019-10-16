b2a5a0f041fe675b529beecb11de453af4e5b07c
Lucas Graybuck <lucasg@alleninstitute.org>
2019-10-15 17:55:22 -0700

    adding optparse-based options to make command-line arguments easier to use.

--- run_hto_processing.R
+++ run_hto_processing.R
@@ -1,12 +1,47 @@
-args <- commandArgs(trailingOnly = TRUE)
+library(optparse)
+
+option_list <- list(
+  make_option(opt_str = c("-t","--type"),
+              type = "character",
+              default = NULL,
+              help = "Type of HTO results. One of: cite, awk, test_cite, or test_awk",
+              metavar = "character"),
+  make_option(opt_str = c("-i","--in_file"),
+              type = "character",
+              default = NULL,
+              help = "Input HTO result input file",
+              metavar = "character"),
+  make_option(opt_str = c("-k","--in_key"),
+              type = "character",
+              default = NULL,
+              help = "Input HTO name key file",
+              metavar = "character"),
+  make_option(opt_str = c("-m","--out_mat"),
+              type = "character",
+              default = NULL,
+              help = "Output HTO count matrix file",
+              metavar = "character"),
+  make_option(opt_str = c("-t","--out_table"),
+              type = "character",
+              default = NULL,
+              help = "Output HTO category table file",
+              metavar = "character"),
+  make_option(opt_str = c("-o","--out_summ"),
+              type = "character",
+              default = NULL,
+              help = "Output HTML run summary file",
+              metavar = "character"),
+)
+
+args <- parse_args(OptionParser(option_list = option_list))
 
 rmarkdown::render(
   input = "hto_processing.Rmd",
-  output_file = args[6],
-  params = list(in_type = args[1],
-                in_file = args[2],
-                in_key  = args[3],
-                out_mat = args[4],
-                out_tbl = args[5]),
+  params = list(in_type = args$type,
+                in_file = args$in_file,
+                in_key  = args$in_key,
+                out_mat = args$out_mat,
+                out_tbl = args$out_table),
+  output_file = args$out_summ,
   quiet = TRUE
 )
