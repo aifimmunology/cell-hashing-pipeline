aaccef6aabb8eea80324c5c27a166a8090d05fc9
Lucas Graybuck <lucasg@alleninstitute.org>
2019-10-15 17:58:48 -0700

    fix hanging comma, make arguments consistent.

--- run_hto_processing.R
+++ run_hto_processing.R
@@ -1,7 +1,7 @@
 library(optparse)
 
 option_list <- list(
-  make_option(opt_str = c("-t","--type"),
+  make_option(opt_str = c("-t","--in_type"),
               type = "character",
               default = NULL,
               help = "Type of HTO results. One of: cite, awk, test_cite, or test_awk",
@@ -21,27 +21,27 @@ option_list <- list(
               default = NULL,
               help = "Output HTO count matrix file",
               metavar = "character"),
-  make_option(opt_str = c("-t","--out_table"),
+  make_option(opt_str = c("-t","--out_tbl"),
               type = "character",
               default = NULL,
               help = "Output HTO category table file",
               metavar = "character"),
-  make_option(opt_str = c("-o","--out_summ"),
+  make_option(opt_str = c("-o","--out_html"),
               type = "character",
               default = NULL,
               help = "Output HTML run summary file",
-              metavar = "character"),
+              metavar = "character")
 )
 
 args <- parse_args(OptionParser(option_list = option_list))
 
 rmarkdown::render(
   input = "hto_processing.Rmd",
-  params = list(in_type = args$type,
+  params = list(in_type = args$in_type,
                 in_file = args$in_file,
                 in_key  = args$in_key,
                 out_mat = args$out_mat,
-                out_tbl = args$out_table),
-  output_file = args$out_summ,
+                out_tbl = args$out_tbl),
+  output_file = args$out_html,
   quiet = TRUE
 )
