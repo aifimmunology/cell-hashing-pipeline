7c5b1bd0d9ed8045c519592ddb0247cba27fe6de
Lucas Graybuck <lucasg@alleninstitute.org>
2019-11-06 13:40:51 -0800

    Adding wrappers for splitting and merging

---
+++ run_h5_merge_by_hash.R
@@ -0,0 +1,41 @@
+library(optparse)
+
+option_list <- list(
+  make_option(opt_str = c("-i","--in_dir"),
+              type = "character",
+              default = NULL,
+              help = "Input directory containing HDF5 files",
+              metavar = "character"),
+  make_option(opt_str = c("-d","--out_dir"),
+              type = "character",
+              default = NULL,
+              help = "Output Directory",
+              metavar = "character"),
+  make_option(opt_str = c("-o","--out_html"),
+              type = "character",
+              default = NULL,
+              help = "Output HTML run summary file",
+              metavar = "character")
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+
+args <- parse_args(opt_parser)
+
+if(is.null(args$in_type)) {
+  print_help(opt_parser)
+  stop("No parameters supplied.")
+}
+
+file.copy(system.file("rmarkdown/merge_h5_by_hash.Rmd", package = "H5weaver"),
+          "./merge_h5_by_hash.Rmd")
+
+rmarkdown::render(
+  input = "./merge_h5_by_hash.Rmd",
+  params = list(in_dir = args$in_dir,
+                out_dir = args$out_dir),
+  output_file = args$out_html,
+  quiet = TRUE
+)
+
+file.remove("./merge_h5_by_hash.Rmd")
---
+++ run_h5_split_by_hash.R
@@ -0,0 +1,54 @@
+library(optparse)
+
+option_list <- list(
+  make_option(opt_str = c("-i","--in_h5"),
+              type = "character",
+              default = NULL,
+              help = "Input HDF5 file",
+              metavar = "character"),
+  make_option(opt_str = c("-m","--in_mat"),
+              type = "character",
+              default = NULL,
+              help = "Input HTO count matrix file",
+              metavar = "character"),
+  make_option(opt_str = c("-c","--in_tbl"),
+              type = "character",
+              default = NULL,
+              help = "Input HTO category table file",
+              metavar = "character"),
+  make_option(opt_str = c("-d","--out_dir"),
+              type = "character",
+              default = NULL,
+              help = "Output Directory",
+              metavar = "character"),
+  make_option(opt_str = c("-o","--out_html"),
+              type = "character",
+              default = NULL,
+              help = "Output HTML run summary file",
+              metavar = "character")
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+
+args <- parse_args(opt_parser)
+
+if(is.null(args$in_type)) {
+  print_help(opt_parser)
+  stop("No parameters supplied.")
+}
+
+file.copy(system.file("rmarkdown/split_h5_by_hash.Rmd", package = "H5weaver"),
+          "./split_h5_by_hash.Rmd")
+
+rmarkdown::render(
+  input = "./split_h5_by_hash.Rmd",
+  params = list(in_h5 = args$in_h5,
+                in_mat = args$in_mat,
+                in_tbl  = args$in_tbl,
+                well_id = args$in_well,
+                out_dir = args$out_dir),
+  output_file = args$out_html,
+  quiet = TRUE
+)
+
+file.remove("./split_h5_by_hash.Rmd")
